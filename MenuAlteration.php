c8d6a31424a792541a2df61e33a8e6b3611ac1fc
Kate Strombom <kstrombom@ku.edu>
2016-04-07 11:48:34 -0500

    Can now add to the table
    Has a weird add/delete glitch that needs to be figured out

--- MenuAlteration.php
+++ MenuAlteration.php
@@ -1,6 +1,10 @@
 <?php
 
 $menuArray = $_POST["menu"];
+$Name = $_POST["name"];
+$Ingredients = $_POST["ingredients"];
+$Price = $_POST["price"];
+
 
      $mysqli = new mysqli("mysql.eecs.ku.edu", "jdrahoza", "Hello", "jdrahoza");
 
@@ -9,6 +13,22 @@ $menuArray = $_POST["menu"];
             echo "printf('Connect failed: %s\n', $mysqli->connect_error)";
             exit();
         }
+
+        #check table for username
+      	$select = "SELECT * FROM Menu WHERE Name = '$Name'";
+      	$result = $mysqli -> query($select);
+      	#if username is not in table
+      	if($result -> num_rows != 0)
+      	{
+      		echo "<p>Name already exists</p>";
+      	}
+      	#if OK to add
+      	else
+      	{
+      		#add to table
+      		$mysqli -> query ("INSERT INTO Menu (Name,Ingredients,Price) VALUES ('$Name','$Ingredients','$Price')");
+      	}
+
 
         for($i=0; $i<count($menuArray);$i++)
         {
