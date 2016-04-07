b84461e8a59abc296cf4ecccac9adb4c5b76ad45
Kate Strombom <kstrombom@ku.edu>
2016-04-07 10:13:03 -0500

    Start Menu Alteration for admin

---
+++ MenuAlteration.php
@@ -0,0 +1,24 @@
+<?php
+
+$menuArray = $_POST["menu"];
+
+     $mysqli = new mysqli("mysql.eecs.ku.edu", "jdrahoza", "Hello", "jdrahoza");
+
+        if ($mysqli->connect_errno)
+        {
+            echo "printf('Connect failed: %s\n', $mysqli->connect_error)";
+            exit();
+        }
+
+        echo "Deleted the following posts:<br>";
+
+        for($i=0; $i<count($menuArray);$i++)
+        {
+            $delete = "DELETE FROM Menu WHERE Name = '$menuArray[$i]'";
+            $mysqli->query($delete);
+            echo $menuArray[$i];
+        }
+
+       $mysqli->close();
+
+?>
