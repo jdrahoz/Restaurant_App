8da1d6f194b3bfd5bc945ebcf90c376bebcf28ae
Julia Drahozal <jdrahoz@ku.edu>
2016-04-08 15:49:34 -0500

    Update accounting to display between certain times

--- accounting.php
+++ accounting.php
@@ -1,5 +1,12 @@
  <?php
 
+    $start_date = $_POST["start"];
+    $end_date = $_POST["end"];
+
+    if ($end_date < $start_date) {
+        exit ();
+    }
+
     // open mysql
     $connection = new mysqli ("mysql.eecs.ku.edu", "jdrahoza", "Hello", "jdrahoza");
 
@@ -8,13 +15,35 @@
     	echo "connect failed";
     	exit ();
     }
-    //print out the accounting table
-    $query = "SELECT * FROM Accounting";
+
+    // select from accounting table
+    $query = "SELECT * FROM Accounting WHERE Time BETWEEN '$start_date' AND '$end_date'";
     $result = $connection->query($query);
     $num = $result -> num_rows;
 
+    // print in between dates
+    echo "<table>";
+    for ($i = 0; $i < $num; $i++) {
+        $row = $result -> fetch_assoc();
+        $item = $row["Item"];
+        $time = $row["Time"];
+        $alterations = $row ["Alterations"];
+        $price = $row["Price"];
+        $tax = $row["Tax"];
+        $tableNum = $row["TableNum"];
 
+        echo "<tr>";
+        echo "<td>$item</td>";
+        echo "<td>$alterations</td>";
+        echo "<td>$tableNum</td>";
+        echo "<td>$price</td>";
+        echo "<td>$tax</td>";
+        echo "<td>$time</td>";
+        echo "</tr>";
 
+    }
+
+    echo "</table>";
 
     $connection -> close();
 
