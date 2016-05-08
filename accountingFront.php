721c71d1b000286eddc1055274f01173b9b876f6
Quinton Wiley <qtwiley95@gmail.com>
2016-05-07 22:14:01 -0500

    create sorting forms. delete accounting.php

--- accountingFront.php
+++ accountingFront.php
@@ -11,7 +11,53 @@
      echo "Welcome, ";
      echo $_SESSION['login'];
    }
+?>
 
+<form method="post" action="?">
+<select name="option">
+  <option>No Sort</option>
+  <option>Time</option>
+  <option>Popular</option>
+  <option>albert</option>
+  </select>
+  <button type="submit">SUBMIT</button>
+</form>
+
+<?php
+  if(isset($_POST["option"])){
+    $option = $_POST["option"];
+    if($option == "Time"){
+      echo "you selected time";
+      // add form to narrow down dates
+      echo "<form id='form' method='post' action='?'>";
+      echo "<p>start date</p><input type='date' name='start'>";
+      echo "<p>end date</p><input type='date' name='end'>";
+      echo "<input type=submit>";
+      echo "<br>";
+      echo "</form>";
+    }
+    elseif($option == "Popular")
+    {
+      echo "you selected 123123";
+    }elseif($option == "something"){
+
+    }
+  }
+
+
+
+  //if certain post conditions are met call various functions defined below
+if(isset($_POST['start']) && isset($_POST['end']))
+{
+  sortByTime();
+}
+
+?>
+
+
+<?php
+if(!isset($option) || $option == "No Sort")
+{
    $username=$_SESSION['login'];
    $tableName=$username."_Accounting";
 
@@ -31,6 +77,7 @@
    // display table of all ordered items
    $total = 0;
    echo "<table cellspacing=\"10px\">";
+   echo "<caption><b>Table of Every Item Bought In Your Restaurant</b></caption>";
    echo "<tr>";
    echo "<th>Item</th>";
    echo "<th>Alterations</th>";
@@ -65,14 +112,57 @@
    echo "</table>";
 
    $connection -> close();
+}
 
-    // add form to narrow down dates
-    echo "<form id=form method=post action='accounting.php'>";
-    echo "<p>start date</p><input type=date name='start'>";
-    echo "<p>end date</p><input type=date name='end'>";
-    echo "<input type=submit>";
-    echo "<br>";
-    echo "</form>";
+function sortByTime(){
+  $username=$_SESSION['login'];
+  $tableName=$username."_Accounting";
+  $start_date = $_POST["start"];
+  $end_date = $_POST["end"];
+  // open mysql
+  $connection = new mysqli ("mysql.eecs.ku.edu", "jdrahoza", "Hello", "jdrahoza");
+  // check connection
+  if ($connection === false) {
+  	exit ();
+  }
+  // select from accounting table
+  $query = "SELECT * FROM $tableName WHERE theTime BETWEEN '$start_date' AND '$end_date'";
+  $result = $connection->query($query);
+  $num = $result -> num_rows;
+  // print in between dates
+  echo "<table cellspacing=\"10px\">";
+  echo "<tr>";
+  echo "<th>Item</th>";
+  echo "<th>alterations</th>";
+  echo "<th>tableNum</th>";
+  echo "<th>price</th>";
+  echo "<th>tax</th>";
+  echo "<th>time</th>";
+  echo "</tr>";
+  for ($i = 0; $i < $num; $i++) {
+    $row = $result -> fetch_assoc();
+    $item = $row["Item"];
+    $time = $row["theTime"];
+    $alterations = $row ["Alterations"];
+    $price = $row["Price"];
+    $tax = $row["Tax"];
+    $tableNum = $row["TableNum"];
+
+    echo "<tr>";
+    echo "<td>$item</td>";
+    echo "<td>$alterations</td>";
+    echo "<td>$tableNum</td>";
+    echo "<td>$price</td>";
+    echo "<td>$tax</td>";
+    echo "<td>$time</td>";
+    echo "</tr>";
+  }
+  echo "</table>";
+  $connection -> close();
+}//End sortByTime
+
+
+
 
 ?>
 
