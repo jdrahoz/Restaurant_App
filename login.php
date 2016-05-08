c1ab1749521ad440bb776209d95f4f71bb313e15
Quinton Wiley <qtwiley95@gmail.com>
2016-05-08 14:32:23 -0500

    change text field to password field

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
 
     <form action="?" id="login" method = "post">
          Username   <input type="text" name="username" ><br>
-         Password   <input type="text" name="password" ><br>
+         Password   <input type="password" name="password" ><br>
     <input type="submit" value="Submit">
     </form>
 
